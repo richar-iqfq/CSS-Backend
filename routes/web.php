ae9fc8ea216518f6796778cc406a39b421dfc21f
richar-iqfq <ricardoambmed@gmail.com>
2023-03-23 16:04:16 -0600

    Primeros pasos sobre controladores

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home';
+});
+
+Route::get('/usuarios', 'UserController@index');
+
+Route::get('/usuarios/nuevo', function () {
+    return 'Crear nuevo usuario';
+});
+
+Route::get('/usuarios/{id}', function ($id) {
+    return "Mostrando detalle del usuario: {$id}";
+})->where('id', '\d+');
+
+Route::get('/saludo/{name}/{nickname?}', function($name, $nickname=null) {
+    $name = ucfirst($name);
+
+    if ($nickname) {
+        return "Bienvenido {$name}, tu apodo es {$nickname}";
+    } else {
+        return "Bienvenido {$name}";
+    }
 });
