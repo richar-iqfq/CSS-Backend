7e109c10f9d94bba0ee63a8fefb2d11cefe0c7a1
richar-iqfq <ricardoambmed@gmail.com>
2023-03-30 17:25:57 -0600

    Revert "Primeros pasos sobre controladores"
    This reverts commit ae9fc8ea216518f6796778cc406a39b421dfc21f.

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Home';
-});
-
-Route::get('/usuarios', 'UserController@index');
-
-Route::get('/usuarios/nuevo', function () {
-    return 'Crear nuevo usuario';
-});
-
-Route::get('/usuarios/{id}', function ($id) {
-    return "Mostrando detalle del usuario: {$id}";
-})->where('id', '\d+');
-
-Route::get('/saludo/{name}/{nickname?}', function($name, $nickname=null) {
-    $name = ucfirst($name);
-
-    if ($nickname) {
-        return "Bienvenido {$name}, tu apodo es {$nickname}";
-    } else {
-        return "Bienvenido {$name}";
-    }
+    return view('welcome');
 });
